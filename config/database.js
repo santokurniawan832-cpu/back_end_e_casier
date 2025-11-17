caf46d6c5a9e5c1bbbb9d956725d4039f75cb095
julie-12rpl <ksj885709@gmail.com>
2025-11-17 15:14:52 +0700

    menambahkan logging untuk false atau mematikan didalam terminal

--- config/database.js
+++ config/database.js
@@ -10,7 +10,8 @@ const sequelize = new Sequelize(
     process.env.DB_PASS, // pengisian nilai password database
    {
     host:   process.env.DB_HOST, // pengisian nilai host database
-    dialect: process.env.DB_CONNECTION // pengisian nilai jenis koneksi database
+    dialect: process.env.DB_CONNECTION, // pengisian nilai jenis koneksi database
+    logging: false // mematikan loggin diconsole agar 
 })
 
 // melakukan export  modul objek sequelize
